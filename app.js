78395a25f3034dec696684a79be1315008bcf56a
Vishrutha Abbaiah <vishruthaabbaiah@Vishruthas-MacBook-Pro.local>
2022-11-19 21:34:47 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors'
+import HelloController
+    from "./controllers/hello-controller.js"
+import UserController
+    from "./controllers/users/users-controller.js"
+import TuitsController
+    from "./controllers/tuits/tuits-controller.js";
+const app = express()
+app.use(cors())
+//app.get('/hello', (req, res) => {res.send('Life is good!')})
+//app.get('/', (req, res) => {res.send('Welcome to Full Stack Development!')})
+app.use(express.json());
+TuitsController(app);
+HelloController(app)
+UserController(app)
+app.listen(process.env.PORT || 4000)
